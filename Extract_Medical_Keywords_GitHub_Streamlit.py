3170e6400b8b9a4e88336450e3be1901ecc22e24
cplooo <cplo@kimo.com>
2024-07-10 17:24:02 +0800

    Add files via upload

--- Extract_Medical_Keywords_GitHub_Streamlit.py
+++ Extract_Medical_Keywords_GitHub_Streamlit.py
@@ -60,11 +60,11 @@ def download_mesh_data():
                 file.write(response.content)
             st.write("下載完成")
 
-            # 檢查文件內容是否正確
-            with open(file_path, 'r', encoding='utf-8') as file:
-                first_lines = ''.join([file.readline() for _ in range(10)])
-                st.write("文件頭部內容:")
-                st.text(first_lines)
+            # # 檢查文件內容是否正確
+            # with open(file_path, 'r', encoding='utf-8') as file:
+            #     first_lines = ''.join([file.readline() for _ in range(10)])
+            #     st.write("文件頭部內容:")
+            #     st.text(first_lines)
 
         except requests.exceptions.RequestException as e:
             st.error(f"下載MeSH文件時發生錯誤: {e}")
